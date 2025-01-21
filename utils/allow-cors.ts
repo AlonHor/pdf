09ee2acd95269429658c38adec28e9c66df3027f
Alon <alon.horesh.08@gmail.com>
2025-01-21 20:59:21 +0200

    fix: remove unused headers from CORS

--- utils/allow-cors.ts
+++ utils/allow-cors.ts
@@ -1,8 +1,6 @@
 const allowCors = (handler: any) => async (req: any, res: any) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins or specify a domain
-  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
 
   if (req.method === "OPTIONS") {
     res.status(200).end();
